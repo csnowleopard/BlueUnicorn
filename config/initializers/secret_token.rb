b9c7d72f6955673ecfc03a3915727cb42ff52d63
csnowleopard <csnowleopard@gmail.com>
2014-03-02 02:20:43 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlueUnicorn::Application.config.secret_key_base = '4e100e754d8af01f352a65eca1ec469612088ffbb9ae39bea9fbda5c5849cd11c2dc9b35c307a21caca04fdf8a80bdea6f9d9fd85b58ca23b576d2f7a93ab008'
